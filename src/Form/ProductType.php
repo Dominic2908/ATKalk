5676cfceab6e88215f8f4c58b3cdc6a45584224e
Dominic2908 <dominic.klingenberg@gmx.de>
2023-12-07 07:15:50 +0100

    WIP 07.12.2023

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -6,7 +6,9 @@ use App\Entity\Product;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 
@@ -15,7 +17,11 @@ class ProductType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('productName')
+        ->add('productName', TextType::class, [
+                'label_attr' => ['class' => 'MYCLASSFOR_LABEL'],
+                'attr' => ['class' => 'product_name']
+                    
+                ])
 
             ->add('productType', ChoiceType::class, [
                 'choices' => [
@@ -23,13 +29,41 @@ class ProductType extends AbstractType
                     'Produkt' => 'Produkt',
                 ],
             ])
-            ->add('description')
-            ->add('taxRate')
-            ->add('serialNumber')
-            ->add('intNumber')
-            ->add('storagePlace')
-            ->add('quantity')
-            ->add('price')
+            ->add('description', TextType::class, [
+                'label_attr' => ['class' => 'MYCLASSFOR_LABEL'],
+                'attr' => ['class' => 'product_name']
+                
+            ])
+            ->add('taxRate', IntegerType::class, [
+                'label_attr' => ['class' => 'MYCLASSFOR_LABEL'],
+                'attr' => ['class' => 'product_name']
+                
+            ])
+            ->add('serialNumber', IntegerType::class, [
+                'label_attr' => ['class' => 'MYCLASSFOR_LABEL'],
+                'attr' => ['class' => 'product_name']
+                
+            ])
+            ->add('intNumber', IntegerType::class, [
+                'label_attr' => ['class' => 'MYCLASSFOR_LABEL'],
+                'attr' => ['class' => 'product_name']
+                
+            ])
+            ->add('storagePlace', TextType::class, [
+                'label_attr' => ['class' => 'MYCLASSFOR_LABEL'],
+                'attr' => ['class' => 'product_name']
+                
+            ])
+            ->add('quantity', IntegerType::class, [
+                'label_attr' => ['class' => 'MYCLASSFOR_LABEL'],
+                'attr' => ['class' => 'product_name']
+                
+            ])
+            ->add('price', IntegerType::class, [
+                'label_attr' => ['class' => 'MYCLASSFOR_LABEL'],
+                'attr' => ['class' => 'product_name']
+                
+            ])
             ->add('save', SubmitType::class, [
                 
             ])
